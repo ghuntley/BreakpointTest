a15ca3b9d94b58619f92a8ad889e4b3c6a0f677e
Geoffrey Huntley <ghuntley@ghuntley.com>
2021-03-13 09:13:37 +1100

    breakpoints functioning in a console app using vsdbg

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BreakpointTest
+{
+    class Program
+    {
+        private static string x;
+        static void Main(string[] args)
+        {
+
+            x = "hello";
+
+            x = "world";
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
